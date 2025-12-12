3f72157c0e4d7dfc8a9e4c8c4176ea63e84cade6
hayat <142229990+hyuyt@users.noreply.github.com>
2025-12-12 10:42:59 +0500

    F commit

---
+++ src/Owner.java
@@ -0,0 +1,37 @@
+public class Owner {
+    // 1. PRIVATE FIELDS (minimum 4)
+    private Type field1;
+    private Type field2;
+    private Type field3;
+    private Type field4;
+    // 2. CONSTRUCTOR WITH PARAMETERS
+    public ClassName(Type field1, Type field2, Type field3, Type field4) {
+// TODO: Initialize all fields using "this" keyword
+    }
+    // 3. DEFAULT CONSTRUCTOR (optional)
+    public ClassName() {
+// TODO: Set default values
+    }
+    // 4. GETTERS (one for each field)
+    public Type getField1() {
+// TODO: Return field1
+    }
+    // ... more getters
+// 5. SETTERS (one for each field)
+    public void setField1(Type field1) {
+// TODO: Use "this.field1 = field1"
+    }
+    // ... more setters
+// 6. ADDITIONAL METHODS (minimum 2)
+    public void methodName1() {
+// TODO: Add your logic here
+    }
+    public ReturnType methodName2() {
+// TODO: Add your logic here
+    }
+    // 7. toString() METHOD
+    @Override
+    public String toString() {
+// TODO: Return string with all field values
+    }
+}
